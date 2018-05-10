41cfea6469f24edf0f19f9092a4b2de1bdf2c508
Juliana Kang <julianajlk@gmail.com>
2018-05-10 13:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
-function happyHolidays(string) {
+function happyHolidays() {
   return "Happy Holidays!"
 }
 
-function happyHolidaysTo(string) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
